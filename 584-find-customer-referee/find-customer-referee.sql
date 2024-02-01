5e680189e36501c17398f5f1710fb6604d2cbfff
Dhiyaneshwar G <59414105+Dhiyaneshwar05@users.noreply.github.com>
2024-02-01 11:54:22 +0530

    Time: 794 ms (83.26%) | Memory: 0B (100.00%) - LeetSync

--- 584-find-customer-referee/find-customer-referee.sql
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -2,3 +2,13 @@
 
 select name from customer
 where referee_id != 2 or referee_id is null;
+
+/*SELECT name
+FROM Customer
+WHERE COALESCE(referee_id,0) <> 2;
+
+<> is same as !=
+
+here COALESCE is used to replace NULL values with zero before checking whether it is equal to 2 or not.*/
+
+#https://www.datacamp.com/tutorial/coalesce-sql-function 
