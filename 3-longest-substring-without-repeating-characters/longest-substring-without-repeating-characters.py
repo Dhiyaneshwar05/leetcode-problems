240e393fc257de9ce71075dcd0378d3c8f3f9b43
Dhiyaneshwar G <59414105+Dhiyaneshwar05@users.noreply.github.com>
2024-06-28 20:24:24 +0530

    Time: 332 ms (8.56%) | Memory: 16.6 MB (39.14%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,30 +1,43 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        #M2 sliding window but clearer version
+        l,r,ml=0,0,0
+        ss=""
+        
+        while r<len(s):
+            if s[r] not in ss:
+                ss+=s[r]
+                r+=1
+            else:#if s[r] in ss
+                ml=max(r-l,ml)#curr_ss_len=r-l
+                ss="" #reset ss
+                l+=1
+                r=l
+        return max(ml,r-l)
 
-        #M1 (can use dict, here i use str)
-        max_len=-1
-        l=""
+
+        # #M1 (can use dict, here i use str)
+        # max_len=-1
+        # l=""
         
-        if len(s)<2: return len(s)
+        # if len(s)<2: return len(s)
         
-        left,right=0,1
-        l+=s[left] #adding the 1st ele 
+        # left,right=0,1
+        # l+=s[left] #adding the 1st ele 
         
-        while right<len(s):  
-            if s[right] not in l:
-                l+=s[right]
+        # while right<len(s):  
+        #     if s[right] not in l:
+        #         l+=s[right]
                 
-            else: #right is pres 
-                
-                if len(l)>max_len:
-                    max_len=len(l)
+        #     else: #right is pres 
+        #         if len(l)>max_len:
+        #             max_len=len(l)
                     
-                l="" #flush out l 
-                left_pos=s.index(s[right])+1
-                l+=s[left_pos]#push that ele to l 
-                s=s[left_pos:]
-                right=0#right wil get +1 by default 
-                
-            right+=1
+        #         l="" #flush out l 
+        #         left_pos=s.index(s[right])+1
+        #         l+=s[left_pos]#push that ele to l 
+        #         s=s[left_pos:]
+        #         right=0 
+        #     right+=1
            
-        return max(max_len,len(l)) #here again chk max as the last formed string may be the lonmgest EX: "dvdf" 
+        # return max(max_len,len(l)) #here again chk max as the last formed string may be the lonmgest EX: "dvdf" 
