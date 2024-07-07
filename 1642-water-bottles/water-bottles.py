bc643f066b84855df8c310332020bdfe849ffe82
Dhiyaneshwar G <59414105+Dhiyaneshwar05@users.noreply.github.com>
2024-07-07 23:58:38 +0530

    Time: 55 ms (5.97%) | Memory: 16.5 MB (32.78%) - LeetSync

--- 1642-water-bottles/water-bottles.py
+++ 1642-water-bottles/water-bottles.py
@@ -8,5 +8,22 @@ class Solution:
         #     rest_empty=numBottles%numExchange
         #     numBottles+=rest_empty
         # return drank
-        return numBottles + (numBottles-1)//(numExchange-1)
 
+        # M1
+        # return numBottles + (numBottles-1)//(numExchange-1)
+
+        # 9 bottles, 3 exchange 3 times, recieve 3
+        # 0 bottles + 3 recieved from last time, 3 exchange, recieve 1
+        # 0 bottles + 1 recieved from last time, 0 exchange, recieve 0
+
+        # start at 9 bottles
+        # + 3 recieved
+        # + 1 recieved
+        # = 13
+        res = numBottles
+        while numBottles >= numExchange:
+            recieved = numBottles % numExchange
+            numBottles //= numExchange
+            res += numBottles
+            numBottles += recieved # add the remainder to the number of bottles for the next calculation to take place
+        return res
