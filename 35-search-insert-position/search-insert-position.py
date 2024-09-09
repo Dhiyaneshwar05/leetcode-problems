c528df8b602cd5f21b2400bb84f1d2519123ec26
Dhiyaneshwar G <59414105+Dhiyaneshwar05@users.noreply.github.com>
2024-09-09 21:48:02 +0530

    Time: 36 ms (99.11%) | Memory: 17.2 MB (37.64%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -1,16 +1,14 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        l=0
-        h=len(nums)-1
-
+        #NOTE : this is same as lb
+        #if same ele present-insert @ first index,
+        #if not presnt insert @ lowerbound (just higher than)
+        l,h=0,len(nums)-1
+        pos=len(nums)#hyp index
         while l<=h:
-            m=(l+h)//2
-
-            if nums[m]==target:
-                return m
-            elif nums[m]>target:
-                h=m-1
-            else:
-                l=m+1
-
-        return l
+            mid=(l+h)//2
+            if nums[mid]>=target: 
+                pos=mid 
+                h=mid-1
+            else:l=mid+1 
+        return pos 
